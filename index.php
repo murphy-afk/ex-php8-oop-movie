cb6af5b9a96ca8fdbb8ee0eee7586ff3f7fb6143
murphy-afk <bardarenoemi@gmail.com>
2026-03-17 10:43:40 +0100

    feat: isLongMovie method

--- index.php
+++ index.php
@@ -3,17 +3,29 @@
     public $title;
     public $description;
     public $releaseYear;
+    public $duration;
 
-    function __construct($_title, $_description, $_releaseYear) {
+    function __construct($_title, $_description, $_releaseYear, $_duration) {
       $this->title = $_title;
       $this->description = $_description;
       $this->releaseYear = $_releaseYear;
+      $this->duration = $_duration;
     }
+
+    public function isLongMovie() {
+      return $this->duration > 120;
+    }
+
+
   }
 
-  $scream = new Movie('Scream','movie desc',1996);
+  $scream = new Movie('Scream','movie desc',1996, 110);
   
   var_dump($scream);
+  if ($scream->isLongMovie()) {
+    echo 'this movie is over two hours long';
+  }
+  else echo 'this movie is under two hours long';
 
 ?>
 
