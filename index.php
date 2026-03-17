9e30b36bd1e8c46dbfc1eba8d615fba99e7854ee
murphy-afk <bardarenoemi@gmail.com>
2026-03-17 11:13:54 +0100

    feat: added genre to movies

--- index.php
+++ index.php
@@ -5,15 +5,15 @@ class Movie
   public $description;
   public $releaseYear;
   public $duration;
-
   public $genre;
 
-  function __construct($_title, $_description, $_releaseYear, $_duration)
+  function __construct($_title, $_description, $_releaseYear, $_duration, Genre $_genre)
   {
     $this->title = $_title;
     $this->description = $_description;
     $this->releaseYear = $_releaseYear;
     $this->duration = $_duration;
+    $this->genre =$_genre;
   }
 
   public function isLongMovie()
@@ -36,16 +36,16 @@ class Genre
     }
 }
 
-$scream = new Movie('Scream', 'A masked killer terrorizes a group of teens in a small town.', 1996, 110);
-$theMatrix = new Movie('The Matrix', 'A hacker discovers the truth about reality.', 1999, 136);
-$interstellar = new Movie('Interstellar', 'A team travels through a wormhole to save humanity.', 2014, 169);
-$theGodfather = new Movie('The Godfather', 'The aging patriarch of a crime family transfers control to his son.', 1972, 175);
-$toyStory = new Movie('Toy Story', 'A cowboy doll feels threatened by a new space ranger toy.', 1995, 81);
-$parasite = new Movie('Parasite', 'A poor family schemes to infiltrate a wealthy household.', 2019, 132);
-
 $horror = new Genre('Horror', 'Stories designed to scare, shock, or unsettle the audience.', 'tense');
 $scienceFiction = new Genre('Science Fiction', 'Narratives exploring futuristic technology, space, or alternate realities.', 'thought-provoking');
 $drama = new Genre('Drama', 'Character-driven stories focused on emotional themes and personal conflict.', 'serious');
+
+$scream = new Movie('Scream', 'A masked killer terrorizes a group of teens in a small town.', 1996, 110, $horror);
+$theMatrix = new Movie('The Matrix', 'A hacker discovers the truth about reality.', 1999, 136, $scienceFiction);
+$interstellar = new Movie('Interstellar', 'A team travels through a wormhole to save humanity.', 2014, 169, $scienceFiction);
+$theGodfather = new Movie('The Godfather', 'The aging patriarch of a crime family transfers control to his son.', 1972, 175, $drama);
+$toyStory = new Movie('Toy Story', 'A cowboy doll feels threatened by a new space ranger toy.', 1995, 81, $drama);
+$parasite = new Movie('Parasite', 'A poor family schemes to infiltrate a wealthy household.', 2019, 132, $drama);
 
 var_dump($horror);
 
@@ -75,7 +75,7 @@ $movies = [
   <p>
     <?php
     foreach ($movies as $movie) {
-  echo $movie->title . ' - ' . $movie->description .' - ' . $movie->releaseYear . ' - ' . ($movie->isLongMovie() ? 'over two hours long' : 'under two hours long') . "<br>";
+  echo $movie->title . ' - ' . $movie->genre->name . ' - ' . $movie->description .' - ' . $movie->releaseYear . ' - ' . ($movie->isLongMovie() ? 'over two hours long' : 'under two hours long') . "<br>";
 }
 
     ?>
